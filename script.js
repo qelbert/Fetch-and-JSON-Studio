6254320d479e7ded69c5010716f39586b2d1d2a1
Queue <elbertq2@gmail.com>
2020-08-27 20:22:42 -0500

    updated index and external js file

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// TODO: add code here
+// // TODO: add code here
+
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+      response.json().then(function(json) {
+        const destination = document.getElementById("container");
+        for (let i=0; i<json.length; i++) {
+          let obj = json[i];
+          let section = `
+            <div class="astronaut">
+              <div class="bio">
+                <h3>${obj.firstName} ${obj.lastName} </h3>
+                <ul>
+                  <li>Hours in space: ${obj.hoursInSpace}</li>
+                  <li>Active: ${obj.active}</li>
+                  <li>Skills: ${obj.skills}</li>
+                  </ul>
+              </div>
+              <img class="avatar" src="${obj.picture}">
+            </div> 
+            `   
+            destination.innerHTML += section;
+        }
+      });
+    });
+  });
